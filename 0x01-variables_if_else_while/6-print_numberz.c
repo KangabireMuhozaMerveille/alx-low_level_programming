8936d793682e6abbd09a44461bda3dea480bd16d
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-03-20 13:23:36 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print 0 to 9 followed by a new line
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i)
+	}
+	putchar('\n')
+	return (0)
+}
