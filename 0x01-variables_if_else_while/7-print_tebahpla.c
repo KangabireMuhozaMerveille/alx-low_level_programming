fdd928714205e6d6b4016bda9def1f9a0a4fb37b
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-03-20 13:55:13 -0700

    tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,17 +5,17 @@
 /**
  * main - prints lowercase alphabets followed by a new line.
  *
- * Return: 0 
+ * Return: 0
  */
 int main(void)
 {
 	char alp[26] = "zyxwvutsrqponmlkjihgfedcba";
 	int i;
-	
+
 	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
-}	
+}
