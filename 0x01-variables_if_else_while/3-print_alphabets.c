dda4f8d959e9ee852e05c7b510f6b7dc4472c75c
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-03-20 09:31:41 -0700

    alpHabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,7 @@
  */
 int main(void)
 {
-	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
 
-	for (i = 0; i < 52; i++)
-	{
-		putchar(alp[i]);
-	}
-
-	putchar('\n')
+	putchar(65)
 	return (0);
 }
