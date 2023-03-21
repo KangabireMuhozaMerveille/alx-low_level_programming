a0455b4c8cecde57f0a5dcd75576e06842a3e596
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-03-21 08:30:25 -0700

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase.
- * followed by a new line 
+ * followed by a new line
  */
 void print_alphabet_x10(void)
 {
@@ -22,4 +22,4 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 		i++;
 	}
-}	
+}
