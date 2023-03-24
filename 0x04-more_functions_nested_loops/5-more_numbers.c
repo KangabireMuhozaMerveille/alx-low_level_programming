705d1c5a138d71fd2b5fea4a9d4c0b5ceef42c2d
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-03-24 02:06:43 -0700

    more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,11 @@ void more_numbers(void)
 {
 	int n, x;
 
-	for (n = 0; n < 14; n++)
+	for (n = 0; n < 10; n++)
 	{
 		for (x = 0; x <= 14; x++)
 		{
-			if (x > 0)
+			if (x > 9)
 			{
 				_putchar((x / 10) + '0');
 			}
