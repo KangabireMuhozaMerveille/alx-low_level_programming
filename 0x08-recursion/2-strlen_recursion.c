ce559a509396df6f362ad644a6d4903a80801832
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-04-05 15:25:11 -0700

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - recursively calculates the length of the string
+ * @s: input value
+ * Return: Always 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	return 1 + _strlen_recursion( s + 1);
+}
