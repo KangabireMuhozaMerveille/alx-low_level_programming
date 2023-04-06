e8151fd95d4139ce4a9ed167d56dcec0bf899fc2
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-04-06 15:25:42 -0700

    strlen

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,13 +7,10 @@
 int factorial(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-	int result = 1;
-	if (int i = 1; i <= n; i++)
-	{
-		result *= i;
-	}
-	return result;
+
+	if (n <= 1)
+		return (1);
+
+	return (n * factorial(n - 1));
 }
