2bd98b416e285de9db22d05e2d217bafb94951fa
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-04-06 16:02:42 -0700

    strlen

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,7 +16,7 @@ int _sqrt_recursion(int n)
 /**
  * _sqrt - calculates natural square root
  * @n: number to calculate the square root
- *
+ * @i: the double
  * Return: the natural square root
  */
 int _sqrt(int n, int i)
