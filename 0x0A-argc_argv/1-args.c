fd610451e0e77dd314bbfc0ef27419626c477dc8
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-04-10 15:15:24 -0700

    whate

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,12 +6,11 @@
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0 
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
 	(void) argv; /*Ignore argv*/
 		       printf("%d\n", argc - 1);
-
-		       return (0);
+			return (0);
 }
