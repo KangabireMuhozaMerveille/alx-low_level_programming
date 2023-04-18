9f251aa223cf235c2efab8e37869c1b698ff6121
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-04-17 17:25:06 -0700

    main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * main - prints the outcome
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
-    printf("%s\n",__FILE__);
-    return (0);
-}   
+	printf("%s\n", __FILE__);
+	return (0);
+}
