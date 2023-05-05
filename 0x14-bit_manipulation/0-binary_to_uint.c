73da10f46e7d53a4534ede77592e7bf3998de569
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-05-05 10:31:19 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,16 +6,32 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int a = 0;
-	int i = 0;
+        unsigned int length, powercount, i, j;
+        unsigned int decimal = 0;
+        unsigned int binary = 2;
 
-	while (b[i] != '\0')
-	{
-		if (b[i] != '0' && b[i] != '1' && b == NULL)
-			return 0;
-	}
-	a = (a<<1) | (b[i] - '0');
-	
-	return a;
-	i++;
+        if (b == NULL)
+                return (0);
+        for (j = 0; j < strlen(b); j++)
+        {
+                if (isalpha(b[j]))
+                        return (0);
+        }
+        for (length = strlen(b), i = 0; length > 0; length--, i++)
+        {
+                unsigned int power = 1;
+                char c = b[i];
+
+                if (length == 1)
+                {
+                        decimal += atoi(&c) * 1;
+                        continue;
+                }
+                for (powercount = length - 1; powercount > 0; powercount--)
+                {
+                        power *= binary;
+                }
+                decimal += atoi(&c) * power;
+        }
+        return (decimal);
 }
