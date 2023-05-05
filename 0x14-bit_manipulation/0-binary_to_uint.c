bfff319cc6cfd05a50fb6f2a4265c95b6956aa92
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-05-05 10:41:55 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,32 +6,32 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int length, powercount, i, j;
-        unsigned int decimal = 0;
-        unsigned int binary = 2;
+	unsigned int length, powercount, i, j;
+	unsigned int decimal = 0;
+	unsigned int binary = 2;
 
-        if (b == NULL)
-                return (0);
-        for (j = 0; j < strlen(b); j++)
-        {
-                if (isalpha(b[j]))
-                        return (0);
-        }
-        for (length = strlen(b), i = 0; length > 0; length--, i++)
-        {
-                unsigned int power = 1;
-                char c = b[i];
+	if (b == NULL)
+		return (0);
+	for (j = 0; j < strlen(b); j++)
+	{
+		if (isalpha(b[j]))
+			return (0);
+	}
+	for (length = strlen(b), i = 0; length > 0; length--, i++)
+	{
+		unsigned int power = 1;
+		char c = b[i];
 
-                if (length == 1)
-                {
-                        decimal += atoi(&c) * 1;
-                        continue;
-                }
-                for (powercount = length - 1; powercount > 0; powercount--)
-                {
-                        power *= binary;
-                }
-                decimal += atoi(&c) * power;
-        }
-        return (decimal);
+		if (length == 1)
+		{
+			decimal += atoi(&c) * 1;
+			continue;
+		}
+		for (powercount = length - 1; powercount > 0; powercount--)
+		{
+			power *= binary;
+		}
+		decimal += atoi(&c) * power;
+		}
+	return (decimal);
 }
