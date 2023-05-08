2ee73462cb5a91a0186387b13d7ffec552b15a97
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-05-08 16:08:51 -0700

    cp

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,12 @@
 #include <stddef.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
