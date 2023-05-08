4fd3ff37188c0a38ea668e8ef0952853eaa9e524
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-05-08 10:03:48 -0700

    create

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,11 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 int main(int ac, char **av);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif
